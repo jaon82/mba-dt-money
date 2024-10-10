0eb12c11a27f24ef1fec9609b8a98c1c73097c30
Jean Nunes <jean.nunes@uzzipay.com>
2024-10-10 13:52:23 -0400

    Set axios

--- src/contexts/TransactionContext.tsx
+++ src/contexts/TransactionContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../lib/axios";
 
 interface Transaction {
   id: number;
@@ -24,13 +25,12 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   async function fetchTransactions(query?: string) {
-    const fetchURL = new URL("http://localhost:3333/transactions");
-    if (query) {
-      fetchURL.searchParams.append("q", query);
-    }
-    const response = await fetch(fetchURL);
-    const data = await response.json();
-    setTransactions(data);
+    const response = await api.get("transactions", {
+      params: {
+        q: query,
+      },
+    });
+    setTransactions(response.data);
   }
 
   useEffect(() => {
